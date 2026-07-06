79ff0bb466e45d94abb08348c158292f45b820ee
Nicolle Cassiano <niccassiano@MacBook-Air-de-Nicolle.local>
2026-07-06 19:33:24 -0300

    feat: implementa menus do sistema

--- c/conversor.c
+++ c/conversor.c
@@ -1,12 +1,52 @@
 #include <stdio.h>
 
-// menus - visualizacao 
-void menuPrincipal ();
-void menuDistancia ();
-void menuTemperatura ();
-void menuMoedas ();
+// MENUS DO SISTEMA 
+void menuPrincipal (){
 
-// funcoes / conversoes 
+    printf("=========================================\n");
+    printf("           CONVERSOR-UNIVERSAL           \n");
+    printf("=========================================\n");
+    printf(" 1. Conversão de Distância (Km/Milhas)  \n");
+    printf(" 2. Conversão de Temperatura (C°/F°)    \n");
+    printf(" 3. Conversão de Moedas (Real/Dolar)    \n");
+    printf(" --------------------------------------- \n");
+    printf(" 0. Sair do Programa                     \n");
+    printf("=========================================\n");
+    printf(" Escolha uma opção: ");
+
+}
+void menuDistancia (){
+
+    printf("===== DISTANCIA =====\n");
+    printf("--- Km <-> Milhas ---\n");
+
+    printf("1 - Km -> Milhas\n ");
+    printf("2 - Milhas -> Km\n");
+    printf("3 - Voltar ao Menu Principal\n");
+
+}
+void menuTemperatura (){
+
+    printf("===== TEMPERATURA =====\n");
+    printf("--- Celsius <-> Fahrenheit ---\n");
+
+    printf("1 - Celsius -> Fahrenheit\n");
+    printf("2 - Fahrenheit -> Celsius\n");
+    printf("3 - Voltar ao Menu Principal\n");
+
+}
+void menuMoedas (){
+
+    printf("===== MOEDAS =====\n");
+    printf("--- Real <-> Dolar ---\n");
+
+    printf("1 - Real -> Dolar\n");
+    printf("2 - Dolar -> Real\n");
+    printf("3 - Voltar ao Menu Principal\n");
+
+}
+
+// CONVERSOES
 float celsiusParaFahrenheit(float c);
 float fahrenheitParaCelsius(float f);
 
@@ -16,6 +56,7 @@ float milhasParaKm(float milhas);
 float realParaDolar(float real);
 float dolarParaReal(float dolar);
 
+// MAIN
 int main () {
 
 }
