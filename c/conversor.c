1efc8f7647c336413d6bf07d1a4585cdcadbbd30
Nicolle Cassiano <niccassiano@MacBook-Air-de-Nicolle.local>
2026-07-06 18:51:46 -0300

    adicionado protótipos de função

---
+++ c/conversor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// menus - visualizacao 
+void menuPrincipal ();
+void menuDistancia ();
+void menuTemperatura ();
+void menuMoedas ();
+
+// funcoes / conversoes 
+float celsiusParaFahrenheit(float c);
+float fahrenheitParaCelsius(float f);
+
+float kmParaMilhas(float km);
+float milhasParaKm(float milhas);
+
+float realParaDolar(float real);
+float dolarParaReal(float dolar);
+
+int main () {
+
+}
